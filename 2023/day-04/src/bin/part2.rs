3875f4f5076039eddf1110b595cad322d4cba69e
dork <38471874+qt-dork@users.noreply.github.com>
2023-12-04 21:43:04 -0600

    god this sucked

--- 2023/day-04/src/bin/part2.rs
+++ 2023/day-04/src/bin/part2.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use itertools::Itertools;
 
 fn main() {
@@ -16,16 +18,28 @@ impl Card {
     /// Scores a card by:
     ///
     /// 1. Finding winners
-    /// 2. Doubling 1 by the number of points (2 ^ (points - 1))
-    fn score(&self) -> color_eyre::Result<usize> {
+    fn points(&self) -> color_eyre::Result<usize> {
         let count: usize = self
             .winners
             .iter()
             .filter(|item| self.picks.iter().any(|w| &w == item))
             .count();
-        let count: u32 = count.try_into()?; // lol panics if the number's too big
-        Ok(2usize.pow(count) / 2)
+        Ok(count)
     }
+
+    /// Scores a card by:
+    ///
+    /// 1. Finding winners
+    /// 2. Doubling 1 by the number of points (2 ^ (points - 1))
+    // fn score(&self) -> color_eyre::Result<usize> {
+    //     let count: usize = self
+    //         .winners
+    //         .iter()
+    //         .filter(|item| self.picks.iter().any(|w| &w == item))
+    //         .count();
+    //     let count: u32 = count.try_into()?; // lol panics if the number's too big
+    //     Ok(2usize.pow(count) / 2)
+    // }
 
     // Expected input:
     // -> 41 48 83 86 17 | 83 86  6 31 17  9 48 53 <-
@@ -58,15 +72,43 @@ fn parse(i: &str) -> color_eyre::Result<(usize, Card), String> {
 }
 
 fn part1(i: &str) -> String {
-    let score: usize = i
+    let score: Vec<_> = i
         .lines()
         .map(parse)
-        .map_ok(|(_, card)| card.score())
+        .map_ok(|(_, card)| card.points())
         .flatten()
-        .process_results(|iter| iter.sum())
-        .unwrap(); // all that work and then i unwrap lol
+        .collect();
 
-    score.to_string()
+    let unpack_score: Result<Vec<_>, ()> = {
+        match score.iter().any(|item| item.is_err()) {
+            true => Err(()),
+            false => Ok(score
+                .iter()
+                .map(|item| item.as_ref().unwrap().to_owned())
+                .collect()),
+        }
+    };
+    let unpack_score = unpack_score.unwrap(); // i give up
+    let new_score: usize = unpack_score
+        .iter()
+        .scan(VecDeque::from([1usize]), |state, x| {
+            let copies = state.pop_front().unwrap_or(1);
+
+            let push_back = x.checked_sub(state.len()).unwrap_or(0);
+            for _ in 0..push_back {
+                state.push_back(1);
+            }
+
+            (0..*x).for_each(|i| {
+                state[i] += copies;
+            });
+
+            Some(copies)
+        })
+        .sum();
+
+    // score.to_string()
+    new_score.to_string()
 }
 
 #[cfg(test)]
@@ -82,6 +124,6 @@ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
         let result = part1(input);
-        assert_eq!(result, "13");
+        assert_eq!(result, "30");
     }
 }
